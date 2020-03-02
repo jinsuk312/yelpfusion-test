dc04e85e3ff2baf3a284d4e4219f5c7798478b03
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-03-01 18:37:16 -0600

    displaying business info at each route

--- src/components/businesses/Business.js
+++ src/components/businesses/Business.js
@@ -5,12 +5,25 @@ class Business extends Component {
 		this.props.getBusiness(this.props.match.params.id);
 	}
 	render() {
-		const { name } = this.props;
-		console.log(name);
+		const {
+			name,
+			image_url,
+			phone,
+			rating,
+			location,
+			url
+		} = this.props.business;
+		console.log(this.props.business);
 		const { loading } = this.props;
 		return (
 			<Fragment>
 				<h1>{name}</h1>
+				<img src={image_url} alt="business image" />
+				<p>Phone: {phone}</p>
+				<p>Rating: {rating}</p>
+				<p>
+					Website: <a href={url}>Link</a>
+				</p>
 			</Fragment>
 		);
 	}
